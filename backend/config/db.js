eb41519920e219000836ab119d8386c0fa72acbd
saurav kumar <sauravbegusarai4199@gmail.com>
2026-09-22 19:19:17 +0530

    feat: connect backend to MongoDB Atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/football_match_center");
+        await mongoose.connect(process.env.MONGODB_URI);
 
         console.log("MongoDB connected successfully");
     } catch (error) {
